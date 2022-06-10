d961806396be166f9103b634defd81a323449030
ArtemOdonal <torvald2506@gmail.com>
2022-06-10 22:37:03 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -5,8 +5,26 @@ public class FindMaxInSeq {
     public static int max() {
 
         // Put your code here
+        Scanner scaner = new Scanner(System.in);
+        int minValue = 0;
+        int maxValue = 0;
+        boolean zero = false;
 
-        return 0;
+        while(!zero){
+            int userNumber = scaner.nextInt();
+            if(userNumber == 0){
+                zero = true;
+            }else if(userNumber > 0 && userNumber > maxValue ){
+                maxValue =  userNumber;
+            } else if (userNumber < 0 && userNumber >= -1|| userNumber < -900000 ) {
+                minValue = userNumber;
+            }
+        }
+       if (maxValue == 0){
+           return minValue;
+       }else{
+           return maxValue;
+       }
     }
 
     public static void main(String[] args) {
